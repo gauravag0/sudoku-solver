99adc48103d17b0cc56ce6743f0a0a80ac31fa05
Gaurav Agrawal <fitmindbody123@gmail.com>
2021-07-20 01:37:23 +0530

    optimized backtracking

--- script.js
+++ script.js
@@ -1,127 +1,123 @@
-alert("You can make a sudoku puzzle by clicking on each cell and putting a value in it.\n\nClick on solve button for solution to puzzle.");
+sudokuTable = document.getElementById("sudoku");
+document.querySelector("#reset").addEventListener("click", resetSudoku);
+document.querySelector("#solve").addEventListener("click", solveSudoku);
+var numbers = document.querySelectorAll("#inputTable td");
+var cells = document.querySelectorAll("#sudoku td");
 
-window.onload = function() {
-  puzzleTable = document.getElementById('puzzle');
-};
+var value = "";
 
-function resetPuzzle() {    
-  Array.from(document.getElementsByTagName('td'))
-  .forEach(cell =>{ cell.textContent = ''; });
+function setCurrValue(i) {
+  return function () {
+    value = i % 10;
+    console.log(value);
+  };
 }
 
-function isRowValid(row, y) {
-    for(var i=0;i<9;i++)
-      if((puzzleTable.children[0].children[row-1]
-      .children[i].textContent) == y)
-        return 0;    
-    return 1; 
-}
-
-function isColValid(col, y) {
-    for(var i=0;i<9;i++)
-      if((puzzleTable.children[0].children[i]
-      .children[col-1].textContent) == y)
-        return 0;
-    return 1;     
-}
-
-function isBoxValid(row, col, y) {
-    var bx=3*Math.floor(row/3), by=3*Math.floor(col/3);
-    for(var i=0;i<3;i++)
-      for(var j=0;j<3;j++)
-        if((puzzleTable.children[0].children[bx+i]
-        .children[by+j].textContent)==y)
-          return 0;
-    return 1;
-}
-
-function takeInput(obj, row, col) {
-  var y = prompt("Enter a number from 1 to 9");        
-  if(y == ''){
-    alert("Cell is left blank, enter a number from 1 to 9");
-    return;
-  }    
-  if(y<1 || y>9 || ((y-Math.floor(y)) != 0)){
-    alert("Enter an integer number from 1 to 9 only");
-    return;
-  }        
-  if(!isRowValid(row, y)){
-    alert(y+ " is already present in row, enter another number from 1 to 9");          
-    return;
-  }        
-  if(!isColValid(col, y)){
-    alert(y+ " is already present in column, enter another number from 1 to 9"); 
-    return;
-  }        
-  if(!isBoxValid(row-1, col-1, y)){
-    alert(y+ " is already present in colored 3×3 box, enter another number from 1 to 9");
-    return;
-  } 
-  obj.textContent = y;
-}    
-
-function boxClicked(row, col) {    
-  let clickedBox = puzzleTable.children[0].
-  children[row-1].children[col-1]  
-  takeInput(clickedBox,row,col);   
-}
-
-function canPlace(r, c, y){ 
-  for(var i=0;i<9;i++){
-    if(mat[i][c]==y)
-      return 0;
-    if(mat[r][i]==y)
-      return 0;
-  }  
-  var bx=3*Math.floor(r/3);
-  var by=3*Math.floor(c/3);  
-  for(var i=0;i<3;i++)
-    for(var j=0;j<3;j++)
-      if((mat[bx+i][by+j])==y)
-        return 0;
-  return 1;
-}
-
-function solve(r, c){
-    if(r == 9)
-      return 1;
-    if(c == 9)
-      return solve(r+1, 0);
-    if(mat[r][c] != 0)
-      return solve(r, c+1);
-    for(var y=1;y<=9;y++){
-      if(canPlace(r, c, y)){
-        mat[r][c]=y;           
-        var success=solve(r, c+1);
-        if(success)            
-          return 1;
-        mat[r][c]=0;    
+function setValue(i, j) {
+  return function () {
+    var prevValue = cells[i * 9 + j].textContent;
+    var boxidx = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+    if(prevValue!=0)
+      rf[i][prevValue - 1] = cf[j][prevValue - 1] = boxf[boxidx][prevValue - 1] = false;
+    if (value == 0) {
+      cells[i * 9 + j].classList.remove("blueColor");
+      cells[i * 9 + j].textContent = "";
+      mat[i][j] = value;
+    } else if (canPlace(i, j, value)){
+      cells[i * 9 + j].classList.add("blueColor");
+      cells[i * 9 + j].textContent = value;
+      mat[i][j] = value;
+    } else{
+        if(prevValue!=0)
+          rf[i][prevValue - 1] = cf[j][prevValue - 1] = boxf[boxidx][prevValue - 1] = true;
+        alert(
+          "cannot place " + value + " at cell(" + (i + 1) + ", " + (j + 1) + ")"
+        );
       }
-    }
-    return 0;
-}  
+  };
+}
 
-function solveSudoku() {    
-  var k;
-  mat=[];
-  for(var i=0;i<9;i++)
-    mat[i] = [];
-  for(var i=0;i<9;i++)
-    for(var j=0;j<9;j++){
-      k=puzzleTable.children[0].children[i]
-        .children[j].textContent;
-      if(k=='')
-        mat[i][j]=0;
-      else
-        mat[i][j]=k;
+for (var i = 0; i < 10; i++)
+  numbers[i].addEventListener("click", setCurrValue(i + 1));
+
+for (var i = 0; i < 9; i++)
+  for (var j = 0; j < 9; j++)
+    cells[i * 9 + j].addEventListener("click", setValue(i, j));
+
+var mat = new Array(9);
+for (var i = 0; i < 9; i++) 
+  mat[i] = new Array(9);
+var rf = new Array(9);
+for (var i = 0; i < 9; i++) 
+  rf[i] = new Array(9).fill(false);
+var cf = new Array(9);
+for (var i = 0; i < 9; i++) 
+  cf[i] = new Array(9).fill(false);
+var boxf = new Array(9);
+for (var i = 0; i < 9; i++) 
+  boxf[i] = new Array(9).fill(false);
+
+function resetSudoku() {
+  for (var i = 0; i < cells.length; i++){ 
+    cells[i].textContent = "";
+    cells[i].classList.remove("blueColor");
+  }
+  for (var i = 0; i < 9; i++) 
+    for (var j = 0; j < 9; j++){
+      mat[i][j] = 0;
+      rf[i][j] = cf[i][j] = boxf[i][j] = false;
+    }
+}
+
+function canPlace(r, c, y) {
+  var boxidx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+  if (rf[r][y - 1] || cf[c][y - 1] || boxf[boxidx][y - 1]) 
+    return false;
+  rf[r][y - 1] = cf[c][y - 1] = boxf[boxidx][y - 1] = true;
+  return true;
+}
+
+function solve(r, c) {
+  if (r == 9) 
+    return true;
+  if (mat[r][c] != 0) {
+    if (c == 8) 
+      return solve(r + 1, 0);
+    else 
+      return solve(r, c + 1);
+  }
+  var boxidx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+  for (var y = 1; y <= 9; y++) {
+    if (canPlace(r, c, y)) {
+      mat[r][c] = y;
+      var success;
+      if (c == 8) 
+        success = solve(r + 1, 0);
+      else 
+        success = solve(r, c + 1);
+      if (success) 
+        return true;
+      mat[r][c] = 0;
+      rf[r][y - 1] = cf[c][y - 1] = boxf[boxidx][y - 1] = false;
+    }
+  }
+  return false;
+}
+
+function solveSudoku() {
+  for (var i = 0; i < 9; i++)
+    for (var j = 0; j < 9; j++) {
+      var k = cells[i * 9 + j].textContent;
+      if (k == "") mat[i][j] = 0;
+      else mat[i][j] = k;
     }
   var solFound = solve(0, 0);
-  if(!solFound)
-    alert("No solution to this sudoku puzzle found, you can modify or reset this puzzle");
+  if (solFound == false)
+    alert(
+      "No solution to this sudoku sudoku found, you can modify or reset this sudoku"
+    );
   else
-    for(var i=0;i<9;i++)
-      for(var j=0;j<9;j++)
-        puzzleTable.children[0].children[i]
-        .children[j].textContent = mat[i][j];      
+    for (var i = 0; i < 9; i++)
+      for (var j = 0; j < 9; j++) 
+        cells[i * 9 + j].textContent = mat[i][j];
 }
-
